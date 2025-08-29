16f40eb7d2f78dd3d33d17a808cffaae220d3f60
fer0072 <liyaozhang0702@outlook.com>
2025-08-29 16:27:08 +0800

    Add vma debug message.

--- source/vk_engine.cpp
+++ source/vk_engine.cpp
@@ -19,6 +19,14 @@
 #include <glm/gtx/transform.hpp>
 
 #define VMA_IMPLEMENTATION
+#ifdef _DEBUG
+#define VMA_DEBUG_LOG_FORMAT(format, ...)  printf((format), __VA_ARGS__)
+#define VMA_DEBUG_LOG(str)                 printf("%s\n", (str))
+#define VMA_DEBUG_INITIALIZE_ALLOCATIONS 1
+#define VMA_DEBUG_DETECT_CORRUPTION 1
+#define VMA_DEBUG_MARGIN 16
+#define VMA_DEBUG_GLOBAL_MUTEX 1
+#endif
 #include "vk_mem_alloc.h"
 
 constexpr bool bUseValidationLayers = false;
